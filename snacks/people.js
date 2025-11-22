cf5bc31d2cc231f304e1d3c763c5750cc575f2e0
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-22 16:11:19 +0100

    lettura migliore

--- snacks/people.js
+++ snacks/people.js
@@ -12,8 +12,8 @@ const people = [
 
 ];
 
-const canDrive = people.map(person => 
-    person.age >= 18 ? {...person, 'can-drive': 'Può guidare'} : {...person, 'can-drive' : 'Non può guidare'}
-);
+const canDrive = people.map(person => {
+    return person.age >= 18 ? {...person, 'can-drive': 'Può guidare'} : {...person, 'can-drive' : 'Non può guidare'}
+});
 
 console.log(canDrive);

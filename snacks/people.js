0a411be79c5056f59797afa5aaeac3ce32235e03
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-22 16:10:47 +0100

    completato snack sul chi può guidare

---
+++ snacks/people.js
@@ -0,0 +1,19 @@
+const people = [
+
+    {name: 'Giuseppe', surname: 'Lisi', age: '25'},
+    {name: 'Annalisa', surname: 'Moro', age: '25'},
+    {name: 'Alessia', surname: 'Androne', age: '20'},
+    {name: 'Andrea', surname: 'Basili', age: '16'},
+    {name: 'Fabio', surname: 'Moro', age: '25'},
+    {name: 'Riccardo', surname: 'Rossi', age: '14'},
+    {name: 'Carlos', surname: 'Castagnaro', age: '24'},
+    {name: 'Rita', surname: 'Fabiani', age: '16'},
+    {name: 'Carlo', surname: 'Natale', age: '17'}
+
+];
+
+const canDrive = people.map(person => 
+    person.age >= 18 ? {...person, 'can-drive': 'Può guidare'} : {...person, 'can-drive' : 'Non può guidare'}
+);
+
+console.log(canDrive);
